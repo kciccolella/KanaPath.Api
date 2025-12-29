514c696eced97b6dc0e0c443e8288dabd7fb0e00
kciccolella <26233701+kciccolella@users.noreply.github.com>
2025-12-29 15:14:55 -0500

    Add User entity and DbContext wiring

---
+++ Data/KanaPathDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using KanaPath.Api.Models;
+
+namespace KanaPath.Api.Data
+{
+    public class KanaPathDbContext : DbContext
+    {
+        public KanaPathDbContext(DbContextOptions<KanaPathDbContext> options)
+            : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace KanaPath.Api.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
