d1e53ce269eb0927cebbc7787367515682d0413d
kciccolella <26233701+kciccolella@users.noreply.github.com>
2025-12-29 14:00:11 -0500

    Add AuthController with register endpoint

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KanaPath.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("register")]
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+    }
+}
