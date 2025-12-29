39a7c12465e855c1b9908d3a60f623cff81bc6d8
kciccolella <26233701+kciccolella@users.noreply.github.com>
2025-12-29 15:38:50 -0500

    Add initial database migration

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using KanaPath.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<KanaPathDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 
